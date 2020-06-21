7ead0762255cbda8285db228c00b917b3be78325
gcaseli <gcaseli@uolinc.com>
2020-06-21 12:26:55 -0300

    adiciona dependencia jdbc para banco postgres

--- src/main/java/br/com/meli/mlgw/externals/database/RouteRepository.java
+++ src/main/java/br/com/meli/mlgw/externals/database/RouteRepository.java
@@ -1,5 +1,9 @@
 package br.com.meli.mlgw.externals.database;
 
+import br.com.meli.mlgw.entities.Routes;
+import java.util.List;
+
 public interface RouteRepository {
 
+  List<Routes> retrieveRoutes();
 }
--- src/main/java/br/com/meli/mlgw/externals/database/RouteRepositoryImpl.java
+++ src/main/java/br/com/meli/mlgw/externals/database/RouteRepositoryImpl.java
@@ -1,8 +1,24 @@
 package br.com.meli.mlgw.externals.database;
 
+import br.com.meli.mlgw.entities.Routes;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class RouteRepositoryImpl implements RouteRepository{
 
+  @Autowired
+  private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+  public List<Routes> retrieveRoutes() {
+
+    return namedParameterJdbcTemplate.query("select * from route", (rs, rowNumber) -> Routes.valueOf(
+        rs.getInt("id"),
+        rs.getString("path_destination"),
+        rs.getString("origin_ip"),
+        rs.getInt("max_request_per_second")));
+
+  }
 }
