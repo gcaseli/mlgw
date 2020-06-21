2148a73945d645873ce317855367c114b66ba8fe
gcaseli <gcaseli@uolinc.com>
2020-06-21 12:42:54 -0300

    adiciona uri default para redirecionar

--- src/main/java/br/com/meli/mlgw/MlgwApplication.java
+++ src/main/java/br/com/meli/mlgw/MlgwApplication.java
@@ -1,18 +1,48 @@
 package br.com.meli.mlgw;
 
+import br.com.meli.mlgw.entities.Routes;
+import br.com.meli.mlgw.entities.UriMeliConfiguration;
+import br.com.meli.mlgw.externals.database.RouteRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
 import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder.Builder;
 import org.springframework.context.annotation.Bean;
+import reactor.netty.http.client.HttpClient.UriConfiguration;
 
 @SpringBootApplication
+@EnableConfigurationProperties(UriMeliConfiguration.class)
 public class MlgwApplication {
+
+	@Autowired
+	private RouteRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MlgwApplication.class, args);
 	}
+	
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder, UriMeliConfiguration uriConfiguration) {
 
+		List<Routes> routesDatabase = repository.retrieveRoutes();
 
+		String httpUri = uriConfiguration.getHttpbin();
+
+		Builder routes = builder.routes();
+
+		for (Routes r : routesDatabase) {
+			routes
+					.route(p -> p
+							.path(r.getPathDestination())
+							.uri(httpUri));
+		}
+
+		return routes.build();
+	}
 }
---
+++ src/main/java/br/com/meli/mlgw/entities/UriMeliConfiguration.java
@@ -0,0 +1,17 @@
+package br.com.meli.mlgw.entities;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties
+public class UriMeliConfiguration {
+
+  private String httpbin = "http://httpbin.org:80";
+
+  public String getHttpbin() {
+    return httpbin;
+  }
+
+  public void setHttpbin(String httpbin) {
+    this.httpbin = httpbin;
+  }
+}
