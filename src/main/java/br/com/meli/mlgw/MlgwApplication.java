72381e8ab4e3905ef3dd2a2c8454893113993bac
gcaseli <gcaseli@uolinc.com>
2020-06-21 20:41:39 -0300

    adiciona spring docs

--- src/main/java/br/com/meli/mlgw/MlgwApplication.java
+++ src/main/java/br/com/meli/mlgw/MlgwApplication.java
@@ -8,13 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder.Builder;
 import org.springframework.context.annotation.Bean;
-import reactor.netty.http.client.HttpClient.UriConfiguration;
 
 @SpringBootApplication
 @EnableConfigurationProperties(UriMeliConfiguration.class)
@@ -45,4 +42,5 @@ public class MlgwApplication {
 
 		return routes.build();
 	}
+
 }
